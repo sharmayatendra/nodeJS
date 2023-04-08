65a2cdfec37085f65daaff8cb883b38d24dd9a38
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2023-04-09 01:45:42 +0530

    operations: read,write, append , rename

--- index.js
+++ index.js
@@ -16,14 +16,36 @@ console.log("hello world");
 
 // we don't need to specify 'utf-8' in case of write because its by default.
 
+// this is kind of callback hell:
+
 fs.writeFile(
   path.join(__dirname, "write.txt"),
   "i am writing into the file",
   (err) => {
     if (err) throw err;
     console.log("writing into file");
+
+    fs.appendFile(
+      path.join(__dirname, "write.txt"),
+      "\n i am appending in write.txt file",
+      (err) => {
+        if (err) throw err;
+        console.log("data appended");
+
+        fs.rename(
+          path.join(__dirname, "write.txt"),
+          path.join(__dirname, "newWrite.txt"),
+          (err) => {
+            if (err) throw err;
+            console.log("file renamed");
+          }
+        );
+      }
+    );
   }
 );
+
+// if the file doesn't exist in appendFile method it will create the file.
 
 // readFile or methods from node in general are async in nature
 
