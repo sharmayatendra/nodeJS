e7961cc07f6b6765dc1698ffcdb481a2ab1958ae
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2023-04-16 23:06:11 +0530

    server setup

--- server.js
+++ server.js
@@ -14,16 +14,46 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
-  let filePath;
+  server.listen(PORT, () => console.log(`server running at ${PORT}`));
 
-  if (req.url === "/" || req.url === "index.html") {
-    res.statusCode = 200;
-    res.setHeader("Content-type", "text/html");
-    path = path.join(__dirname, "views", "index.html");
-    fs.readFile(path, "utf8", (err, data) => {
-      res.end(data);
-    });
+  const extension = path.extname(req.url);
+
+  let contentType;
+
+  switch (extension) {
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".jpg":
+      contentType = "image/jpeg";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".txt":
+      contentType = "text/plain";
+      break;
+    default:
+      contentType = "text/html";
   }
+
+  // let filePath;
+
+  // switch(req.url){
+  //   case'/':
+  //       res.statusCode = 200
+  //       filePath = path.join(__dirname, 'views', 'index.html')
+  //       fs.readFile(path, 'utf8', (err,data) => {
+  //         res.end(data)
+  //       })
+  //       break;
+  // }
 });
 
 server.listen(PORT, () => console.log(`server running on port: ${PORT}`));
